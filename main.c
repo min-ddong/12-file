46b396ba8f8d602f49461fd4f9092b1dd634f178
min-ddong <jellykim907@gmail.com>
2020-11-23 15:55:39 +0900

    last

--- main.c
+++ main.c
@@ -5,14 +5,37 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 void main(void){
-FILE*fp = NULL;
-char c;
+FILE *fp;
+char str[100];
+char path[100];
+char lookup[100];
+
 fp = fopen("sample.txt", "r");
-if (fp == NULL)
-printf("파일을 못 열음\n");
-while((c=fgetc(fp)) != EOF);
-putchar(c);
+printf(" input file path: ");
+scanf("%s", path);
+printf("input search word :");
+scanf("%s", lookup);
+
+fp = fopen(path, "r");
+if (fp == NULL){{printf("invalid path! (%s)\n", path);
+
+return -1 ;}
+}
+
+
+while( fgets(str, 100, fp) !=NULL)
+{ if (strncmp(str, lookup, strlen(lookup)) == 0){
+	printf("%s", str);
+	printf("search succeed!\n");
+	fclose(fp);
+	return 0;
+}
+}
+
+printf("search failed!\n");
 fclose(fp);
+
+return 0;
 }
 
 
