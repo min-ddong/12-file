77a160341358d706d3664a48c0554e3f4557ac46
min-ddong <jellykim907@gmail.com>
2020-11-23 15:34:58 +0900

    fourth

--- main.c
+++ main.c
@@ -4,17 +4,7 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(void){
-	int i=0;
-char str[4];
-str[0] = 'a';
-str[1] = 'b';
-str[2] = 'c';
-str[3] = '\0';
-printf("%s\n", str);
-
-while (str[i]!= '\0'){
-	printf("%c", str[i]);
-	i = i +1 ;
-}
+void main(void){
+char str[30] = "happy C programming";
+printf("문자열 \"%s\"의 길이 : %i", str, strlen(str));
 }
